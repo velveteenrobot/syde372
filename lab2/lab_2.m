00ece3037de80c05b578a74eb9c828fb9caadb22
Sarah Elliott <sksellio@gmail.com>
2014-03-22 18:20:21 -0400

    Cleaned up the code and fixed the colors a bit. Feel free to tweak.

--- lab2/lab_2.m
+++ lab2/lab_2.m
@@ -2,22 +2,25 @@
 
 close all;
 clear all;
+
+%% 2.4 Non-parametric estimation
+
 load('lab2_1.mat')
 
-[x, dist] = parzen1(a, 0.1, 0.001);
+[x, dist] = parzen1(a, 0.1, 0.1);
 
 figure;
 hold on;
-[f,w] = hist(a,50);
+
+% true density
+[f,w] = hist(a,length(a));
 dx = diff(w(1:2));
-%bar(w,f/sum(f*dx));hold on
+bar(w,f/sum(f*dx), 'y');
 x_axis = min(w):(max(w)-min(w))/(length(dist) - 1):max(w);
+
+%gaussian with given mu, sigma
 normthing = normpdf(x_axis, 5, 1)
-plot(x_axis, normthing)
-plot(x_axis,dist)
-%plot(a)
+plot(x_axis, normthing, 'Color', [1 0 0], 'LineWidth', 2)
 
-
-
-%figure;
-%plot
+%pdf from parzen1
+plot(x_axis,dist, 'LineWidth', 2)
