14cb449d8d9d42899dfc9674e505dcb6b3b5354f
Sarah Elliott <sksellio@gmail.com>
2014-03-23 19:35:02 -0400

    Discriminant stuff for question 4. Still need to do the error analysis.

--- lab2/lab_2.m
+++ lab2/lab_2.m
@@ -24,3 +24,22 @@ plot(x_axis, normthing, 'Color', [1 0 0], 'LineWidth', 2)
 
 %pdf from parzen1
 plot(x_axis,dist, 'LineWidth', 2)
+
+
+%% 4 Sequential Discriminants
+
+step = 1;
+
+load('lab2_3.mat');
+
+figure;
+hold on;
+
+class_A = scatter(a(:,1), a(:,2), 5, ...
+                'r', 'filled');
+
+class_B = scatter(b(:,1), b(:,2), 5, ...
+                'g', 'filled');
+hold on;
+[contour, MED_plots, disc_plot] = sequential_discriminant(step, a, b);
+legend([class_A class_B disc_plot MED_plots(1)],{'Class A', 'Class B', 'Decision Boundary', 'Discriminants'});
---
+++ lab2/sequential_discriminant.m
@@ -0,0 +1,127 @@
+function [disc, MED_plots, disc_plot] = sequential_discriminant(step, A, B)
+    % Classifies two labelled clusters with sequential linear discriminants.
+    % TODO: Return error rates.
+    % --
+    % stepSize = resolution of contour for decision boundary
+    % cA, cB = list of classes for evaluation
+    A_data = A; 
+    B_data = B; 
+    MED_plots = []
+
+    vals = [];
+    vals = [A_data; B_data];
+  
+
+    %extracts min/max x and y from mixed data
+    mins = min(vals);
+    maxs = max(vals);
+
+    x_vals = mins(:,1):step:maxs(:,1);
+    y_vals = mins(:,2):step:maxs(:,2);
+
+    [x,y] = meshgrid(x_vals, y_vals);
+    c = cat(2,x',y');
+    points = reshape(c,[],2);
+
+    
+    A_prototype = []; 
+    B_prototype = [];
+    disc = zeros(length(y_vals), length(x_vals));
+    iter = 0;
+
+    while(~isempty(A_data) && ~isempty(B_data))
+        empty = 'Not empty'
+        A_confusion = [0 0]; B_confusion = [0 0]; 
+
+        while(~isempty(A_confusion) && ~isempty(B_confusion))
+            empty = 'Still not empty'
+            iter = iter + 1
+            A_prototype = A_data(randi(length(A_data(:,1)),1),:); 
+            B_prototype = B_data(randi(length(B_data(:,1)),1),:);
+ 
+            dists = [];
+            
+            A_conf = []; B_conf = [];
+
+            for k=1:length(A_data(:,1))
+                
+                p1 = A_data(k,:);
+                p2 = A_prototype;
+                
+                dist_A_1 = sqrt(sum((p1 - p2) .^ 2));
+                
+                p3 = B_prototype;
+                
+                dist_A_2 = sqrt(sum((p1 - p3) .^ 2));
+                
+                dists = [dist_A_1 dist_A_2]
+                
+                [~, close_class] = min(dists);
+                dists = [];
+
+                if(close_class ~= 1)
+                    A_conf = [A_conf; A_data(k,:)];
+                end
+            end
+            
+        
+            
+            for k=1:length(B_data(:,1))
+                
+                p1 = B_data(k,:);
+                p2 = A_prototype;
+                
+                dist_B_1 = sqrt(sum((p1 - p2) .^ 2));
+                
+                p3 = B_prototype;
+                
+                dist_B_2 = sqrt(sum((p1 - p3) .^ 2));
+                
+                dists = [dist_B_1 dist_B_2];
+                
+                [~, close_class] = min(dists);
+                dists = [];
+
+                if(close_class ~= 2)
+                    B_conf = [B_conf; B_data(k,:)];
+                end
+                
+            end
+            
+            A_confusion = A_conf;
+            B_confusion = B_conf;
+           
+            
+        end
+
+        hold on;
+        [ctr, MED_plot] = MED_points('--b', x_vals, y_vals, points,...
+            A_prototype, B_prototype);
+        MED_plots = [MED_plots MED_plot]; 
+
+        vals2 = [];
+        if(isempty(A_confusion) && isempty(B_confusion)),
+            vals2 = ctr;
+            A_data = A_confusion; B_data = B_confusion;
+        elseif(isempty(A_confusion)),
+            mod = 2;
+            vals2 = (ctr == mod).*mod;
+            B_data = B_confusion;
+        elseif(isempty(B_confusion)),
+            mod = 1;
+            vals2 = (ctr == mod).*mod;
+            A_data = A_confusion;
+        else(~isempty(A_confusion) && ~isempty(B_confusion)), 
+            error('failed.'); 
+        end
+
+        % - Finds all locations in current contour that are 0
+        % - Finds all location in new contour that match our modifier (class)
+        % - Multiplies these together to create a composite contour of
+        %	where our new contour can "fit" in our current contour
+        % - "copies" composite map into our current map.
+        disc = ((disc==0).*(vals2)) + disc;
+    end
+    [c, h] = contour(x_vals, y_vals, disc, 3, '--r');
+    disc_plot = h;
+end
