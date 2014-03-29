f2cc3224d2f5c802b88f08767adc75e6287988e1
Sarah Elliott <sksellio@gmail.com>
2014-03-29 12:58:38 -0400

    First attempt at part 4. Need to check my math. Looks very crappy... :(

---
+++ lab3/lab_3_4.m
@@ -0,0 +1,49 @@
+%From Sonnu's Stuff
+clear;
+clc;
+load feat.mat;
+c8 = Confusion.convertFeatureToClass(f8); % create classes for the n=8 data;
+
+figure;
+
+ft1 = multf8(:,:,1); 
+ft2 = multf8(:,:,2);
+
+combined = [ft1(:), ft2(:)];
+
+dim1 = length(multf8(:,1,1));
+dim2 = length(multf8(1,:,1));
+ctr = zeros(dim1, dim2);
+x_vals = length(multf8(1,:,1));
+
+
+size_class = length(c8);
+dists = [];
+
+x = 1; 
+y = 1;
+
+for i = 1: length(combined(:,1))
+    for j = 1 : size_class
+        dist_mean = combined(i,:) - c8(j).mean; 
+        
+        %Check if this math is right...
+
+        dists = [ dists dist_mean*c8(j).invcovariance*dist_mean'];
+    end
+    
+    [val, close_class] = min(dists);
+    ctr(x,y) = close_class;
+    dists = [];
+
+   if(x == x_vals)
+        x = 1;
+        y = y +1;
+    else
+        x = x +1;
+    end
+end
+
+cimage = ctr;
+
+imagesc(cimage);
