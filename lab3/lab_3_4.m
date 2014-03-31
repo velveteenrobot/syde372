6220ca3b639911a6cc0c54a6c6fafdb0529212e4
Sarah Elliott <sksellio@gmail.com>
2014-03-30 23:06:42 -0400

    FCM stuff

--- lab3/lab_3_4.m
+++ lab3/lab_3_4.m
@@ -37,7 +37,7 @@ for i = 1: length(combined(:,1))
     ctr(x,y) = close_class;
     dists = [];
 
-   if(x == x_vals)
+    if(x == x_vals)
         x = 1;
         y = y +1;
     else
@@ -52,5 +52,62 @@ imagesc(cimage);
 colorbar('YTickLabel',...
     {'Cloth', 'Cotton', 'Grass', 'Pigskin', 'Wood', 'Cork', 'Paper',... 
     'Stone', 'Raiffa', 'Face'});
+
+title('Multim Image Classification');
+
+figure;
+imagesc(multim);
+title('Multim Image');
+colormap(gray);
+
+%%
+clear all;
+close all;
+load feat.mat;
+c8 = Confusion.convertFeatureToClass(f8); % create classes for the n=8 data;
+
+b = 2;
+K = 10;
+
+options = [b, 100, 1e-5, 0];
+[centroids,U,obj_fn] = fcm(f32(1:2,:)',K,options);
+
+figure;
+
+plot(f32(1,:),f32(2,:),'ok');
+maxU = max(U);
+
+% index1 = find(U(1,:)== maxU);
+% index2 = find(U(2,:)== maxU);
+% index3 = find(U(3,:)== maxU);
+% index4 = find(U(4,:)== maxU);
+% index5 = find(U(5,:)== maxU);
+% index6 = find(U(6,:)== maxU);
+% index7 = find(U(7,:)== maxU);
+% index8 = find(U(8,:)== maxU);
+% index9 = find(U(9,:)== maxU);
+% index10 = find(U(10,:)== maxU);
+% 
+% line(f32(1,index1),f32(2,index1),'linestyle','none',...
+%      'marker','*','color','g');
+% line(f32(1,index2),f32(2,index2),'linestyle','none',...
+%      'marker', '*','color','r');
+
+for i = 1 : 10
+    idx = find(U(i,:)== maxU);
+    line(centroids(i,1),centroids(i,2),'linestyle','none',...
+     'marker','*','color',rand(1,3));
+end
+
+title('Fuzzy K-means Prototypesfor K = 10, b = 2');
  
+
+
+figure;
+for i = 1 : 10
+    idx = find(U(i,:)== maxU);
+    line(f32(1,idx),f32(2,idx),'linestyle','none',...
+     'marker','*','color',rand(1,3));
+end 
  
+title('Fuzzy K-means for K = 10, b = 2');
